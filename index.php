6059205a24a9e8e2b1c78847831490a8c967f238
Aleksejs <aleksejs.jevsejevs@gmail.com>
2014-04-18 07:40:51 +0300

    67 page

--- index.php
+++ index.php
@@ -5,4 +5,16 @@
  * Date: 14.18.4
  * Time: 06:17
  */
-phpinfo();
+include('ShopProduct.php');
+include('ShopProductWriter.php');
+include('CDProduct.php');
+include('BookProduct.php');
+
+
+$product1   = new ShopProduct("Собачье сердце", "Михаил", "Булгаков", 5.99);
+$writer     = new ShopProductWriter();
+//$writer->write($product1);
+
+
+$product2 = new ShopProduct("Пропавший без вести", "Группа", "ДДТ", 10.99, null, 60.33);
+print "Исполнитель: {$product2->getProducer()}<br>";
