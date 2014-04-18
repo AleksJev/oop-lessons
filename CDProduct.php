4f4277c5e4e41d7cccab4d0d8caba938676980fa
Aleksejs <aleksejs.jevsejevs@gmail.com>
2014-04-18 10:32:52 +0300

    Add $playLength

--- CDProduct.php
+++ CDProduct.php
@@ -7,6 +7,12 @@
  */
 
 class CDProduct extends ShopProduct {
+    public $playLength;
+
+    function __construct($title, $firstName, $mainName, $price, $playLength) {
+        parent::__construct($title, $firstName, $mainName, $price);
+        $this->playLength = $playLength;
+    }
 
     function getPlayLength() {
         return $this->playLength;
