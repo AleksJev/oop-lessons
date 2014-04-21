4ed4f933cb24d91cfbd30917e73632832e724284
Aleksejs <aleksejs.jevsejevs@gmail.com>
2014-04-22 01:32:04 +0300

    Add function getInstance

--- ShopProduct.php
+++ ShopProduct.php
@@ -12,6 +12,9 @@ class ShopProduct {
     private $producerFirstName;
     protected $price;
     private $discount = 0;
+    private $id = 0;
+    const AVAILABLE = 0;
+    const OUT_OF_STOCK = 1;
 
     public function __construct( $title, $firstName, $mainName, $price) {
         $this->title                = $title;
@@ -49,5 +52,44 @@ class ShopProduct {
         $base  = "{$this->title} ( {$this->producerMainName}, ";
         $base .= "{$this->producerFirstName} )";
         return $base;
+    }
+
+    public function setID($id) {
+        $this->id = $id;
+    }
+
+    public static function getInstance($id, PDO $pdo) {
+        $stmt   = $pdo->prepare("SELECT * FROM products WHERE id=?");
+        $result = $stmt->execute(array($id));
+        $row    = $stmt->fetch();
+
+        if (empty($row)) {return null;}
+        if ($row['type'] == "book") {
+            $product = new BookProduct(
+                $row['title'],
+                $row['firstname'],
+                $row['mainname'],
+                $row['price'],
+                $row['numpages']
+            );
+        } else if ($row['type'] == "cd") {
+            $product = new CDProduct(
+                $row['title'],
+                $row['firstname'],
+                $row['mainname'],
+                $row['price'],
+                $row['playlength']
+            );
+        } else  {
+            $product = new ShopProduct(
+                $row['title'],
+                $row['firstname'],
+                $row['mainname'],
+                $row['price']
+            );
+        }
+        $product->setID($row['id']);
+        $product->setDiscount($row['discount']);
+        return $product;
     }
 } 
