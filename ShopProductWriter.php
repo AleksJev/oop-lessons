2427cf3c87c83a257ccd8834aee0f116739cd7b6
Aleksejs <aleksejs.jevsejevs@gmail.com>
2014-04-22 23:09:36 +0300

    Add abstract class and abstract function

--- ShopProductWriter.php
+++ ShopProductWriter.php
@@ -6,21 +6,13 @@
  * Time: 06:22
  */
 
-class ShopProductWriter {
-    private $products = array();
+abstract class ShopProductWriter {
+    protected $products = array();
 
     public function addProduct( ShopProduct $shopProduct) {
         $this->products[] = $shopProduct;
     }
 
-    public function write() {
-        $str = "";
-        foreach ($this->products as $shopProduct) {
-            $str .= "{$shopProduct->title}: ";
-            $str .= $shopProduct->getProducer();
-            $str .=  " ({$shopProduct->getPrice()}) <br>";
-        }
-        print $str;
-    }
+    abstract public function write();
 
 } 
