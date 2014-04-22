c45a6d172a82fac3e7383ff83953e708480db9b4
Aleksejs <aleksejs.jevsejevs@gmail.com>
2014-04-22 23:02:38 +0300

    First commit

---
+++ XmlProductWriter.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Aleksejs
+ * Date: 14.22.4
+ * Time: 02:22
+ */
+
+class XmlProductWriter extends ShopProductWriter{
+    public function write() {
+        $str    = '<?xml version="1.0" encoding="UTF-8"?>'."\n";
+        $str    .= "<products>\n";
+        foreach ($this->products as $shopProduct) {
+            $str .= "\t<product title=\"{$shopProduct->getTitle()}\">\n";
+            $str .= "\t\t<summary>\n";
+            $str .= "\t\t{$shopProduct->getSummaryLine()}\n";
+            $str .= "\t\t</summary>\n";
+            $str .= "\t</product>\n";
+        }
+        $str .= "</products>\n";
+        print $str;
+    }
+
+} 
